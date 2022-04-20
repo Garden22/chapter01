4215d9334e39a4bc7fc47e3a9b0b059aff8cef26
garden <dalkomjjang@gmail.com>
2022-04-20 13:33:38 +0900

    220420

---
+++ src/com/javaex/ex04/Ex13.java
@@ -0,0 +1,24 @@
+package com.javaex.ex04;
+
+import java.util.Scanner;
+
+public class Ex13 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요[0이면 종료]");
+		
+		int sum = 0;
+		int num;
+		
+		do {
+			num = sc.nextInt();
+			sum += num;
+			System.out.println("합계: " + sum);
+		} while (num != 0);
+		
+		System.out.println("종료");
+		sc.close();
+	}
+}
